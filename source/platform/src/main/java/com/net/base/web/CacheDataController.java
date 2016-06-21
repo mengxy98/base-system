72b2f761f1a0c2a7cbc08fc06ee9ae5f57befe4e
mengxy98 <m541206807@163.com>
2016-06-21 15:40:07 +0800

    Change-Id: Ie5e151ba876d95e64498294f48cabd86a31e284b Signed-off-by: mengxy98 <m541206807@163.com>

--- source/platform/src/main/java/com/net/base/web/CacheDataController.java
+++ source/platform/src/main/java/com/net/base/web/CacheDataController.java
@@ -9,44 +9,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.whalin.MemCached.MemCachedClient;
+import com.net.base.memcache.BaseMemcache;
+import com.net.base.memcache.ModelType;
 
 
-@RequestMapping(value="/memcache")
+@RequestMapping(value="/cache")
 @Controller
-public class Test {
+public class CacheDataController {
 	
 	@Autowired
-	private MemCachedClient memCachedClient;
+	private BaseMemcache baseMemcache;
 	
-	static int i = 1;
-	
-	@RequestMapping(value="/judge.do",method=RequestMethod.GET)
+	@RequestMapping(value="/getDevData.do",method=RequestMethod.GET)
 	@ResponseBody
-	public String returnJsp(Long id){
+	public String getDevData(Object id){
 		String mess="";
 		try {
-			if (null == id || id < 0)return "请传入id";
+			if (null == id)return "请传入id";
 			/**
 			 * 先判断缓存中是否有要取的数据，有的话就取缓存中的数据，没有就调用jdbc取数据库的数据
 			 */
-			System.out.println("memcache缓存的测试"+i++);
-			// 基本类型tostring方法
-			// 通常不需要设置
-			memCachedClient.setPrimitiveAsString(true);
-			Object o = memCachedClient.get(id.toString());
+			String o = baseMemcache.getCacheData(ModelType.DEVICE_MANAGER, id);
 			if (null == o) { //代表缓存中没有这个数据
-				String name = id+"数据库数据"; //存在就返回对应数据，不存在默认返回的是mxy
 				mess = "取到的是数据库的数据"+id;
-				System.out.println("缓存添加数据"+id+memCachedClient.add(id.toString(), name)); //加入到缓存客户端中
+				baseMemcache.inputCacheData(ModelType.DEVICE_MANAGER, id ,mess);
 			}else {
 				//代表缓存中存在缓存的对应数据，从缓存中获取对应数据
-				mess = "取到的是缓存的数据"+o.toString();
-				System.out.println("......取到的缓存数据"+o.toString());
+				mess = o.toString();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return transformData(mess);
+	}
+	
+	public String transformData(String mess) {
 		try {
 			return new String(mess.getBytes("utf-8"), "iso-8859-1");
 		} catch (UnsupportedEncodingException e) {
